e2a69a53cdeecc5310d0e35235a1a66c55c8e7df
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-04-06 03:05:37 +0200

    Add scroll reset after changing route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Translation, LocaleService, TranslationService } from 'angular-l10n';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent extends Translation {
+export class AppComponent extends Translation implements OnInit {
 
-    constructor(public locale: LocaleService, public translation: TranslationService) {
+    constructor(public locale: LocaleService,
+      public translation: TranslationService,
+      private router: Router) {
         super(translation);
 
         this.locale.addConfiguration()
@@ -21,6 +24,19 @@ export class AppComponent extends Translation {
         this.translation.addConfiguration()
             .addProvider('./assets/locales/');
         this.translation.init();
+    }
+
+    ngOnInit(): void {
+        this.resetScroll();
+    }
+
+    resetScroll(): void {
+      this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0)
+        });
     }
 
 }
