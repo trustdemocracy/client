acc3fa9ea8f72e978eeb37a883c5e81a8c03da4f
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-15 19:38:27 +0200

    Add real login

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
-import { User } from "app/_models/user";
-import { Router } from "@angular/router";
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { User } from 'app/_models/user';
+import { environment } from 'app/../environments/environment';
 
 @Injectable()
 export class AuthenticationService {
   private readonly STORAGE_TOKEN_KEY: string = 'userToken';
   private currentToken: string;
 
-  constructor(private router: Router) {
+  constructor(private http: Http) {
     this.currentToken = this.getStoredToken();
   }
 
-  login(user: User, remember: boolean = false) {
-    console.log(user);
+  login(user: User, remember: boolean = false): Observable<boolean> {
+    return this.http.post(environment.usersApi.getToken,
+      JSON.stringify({ username: user.username, password: user.password }))
+      .map((response: Response) => {
+        if (response.ok) {
+          const token = response.json() && response.json().token;
+          if (token) {
+            this.storeToken(token, remember);
+            return true;
+          }
+        }
+
+        return false;
+      });
   }
 
   isLogged(): boolean {
@@ -31,5 +46,13 @@ export class AuthenticationService {
       return sessionToken;
     }
     return localStorage.getItem(this.STORAGE_TOKEN_KEY);
+  }
+
+  private storeToken(token: string, remember: boolean) {
+    if (remember) {
+      localStorage.setItem(this.STORAGE_TOKEN_KEY, token);
+    } else {
+      sessionStorage.setItem(this.STORAGE_TOKEN_KEY, token);
+    }
   }
 }
--- src/app/users/user/login/login.component.ts
+++ src/app/users/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 import { Translation, LocaleService, TranslationService } from 'angular-l10n';
 import { AuthenticationService } from 'app/_services/authentication.service';
 import { User } from "app/_models/user";
@@ -12,12 +13,26 @@ import { User } from "app/_models/user";
 export class LoginComponent extends Translation {
   remember: boolean;
   user: User = new User();
+  error: string = '';
 
-  constructor(public translation: TranslationService, private authenticationService: AuthenticationService) {
+  constructor(
+    public translation: TranslationService,
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {
     super(translation);
   }
 
   onSubmit() {
-    this.authenticationService.login(this.user);
+    this.error = '';
+
+    this.authenticationService.login(this.user)
+      .subscribe((success: boolean) => {
+        if (success) {
+          this.router.navigate(['/']);
+        } else {
+          this.error = this.translation.translate('Invalid credentials');
+        }
+      });
   }
 }
