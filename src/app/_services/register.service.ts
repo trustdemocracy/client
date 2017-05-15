5af01df685cc414767a9a1847c3ea429eb5ffb32
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-15 21:00:28 +0200

    Add real register form

---
+++ src/app/_services/register.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import { User } from 'app/_models/user';
+import { environment } from 'app/../environments/environment';
+
+@Injectable()
+export class RegisterService {
+
+  constructor(private http: Http) {
+  }
+
+  register(user: User, remember: boolean = false): Observable<boolean> {
+    return this.http.post(environment.usersApi.createUser, JSON.stringify(user))
+      .map((response: Response) => {
+        return response.ok;
+      });
+  }
+
+}
--- src/app/users/user/register/register.component.ts
+++ src/app/users/user/register/register.component.ts
@@ -1,16 +1,56 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 import { Translation, LocaleService, TranslationService } from 'angular-l10n';
+import { AuthenticationService } from "app/_services/authentication.service";
+import { RegisterService } from "app/_services/register.service";
+import { User } from "app/_models/user";
 
 @Component({
   selector: 'login',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  providers: [RegisterService]
 })
 export class RegisterComponent extends Translation {
   remember: boolean;
+  user: User = new User();
+  repeatedPassword: string = '';
+  error: string = '';
+  loading: boolean = false;
+  passwordsMatch: boolean = true;
 
-  constructor(public translation: TranslationService) {
+  constructor(
+    public translation: TranslationService,
+    private authenticationService: AuthenticationService,
+    private registerService: RegisterService,
+    private router: Router
+  ) {
     super(translation);
+  }
+
+  onSubmit() {
+    this.error = '';
+    this.loading = true;
+
+    this.registerService.register(this.user)
+      .subscribe((success: boolean) => {
+        if (success) {
+          this.authenticationService.login(this.user, this.remember)
+            .subscribe((loginSuccess: boolean) => {
+              this.loading = false;
+              if (loginSuccess) {
+                this.router.navigate(['']);
+              }
+            });
+        } else {
+          this.loading = false;
+          this.error = this.translation.translate('Failed to create account');
+        }
+      });
+  }
+
+  checkPasswordsMatch(): void {
+    this.passwordsMatch = this.user.password === this.repeatedPassword;
   }
 
 }
