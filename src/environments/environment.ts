ffd20544d9b51863da8a369cffb45f02e3bcf573
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-15 18:48:20 +0200

    Add backend endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,47 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+const usersApiBase = "http://api.trustdemocracy.dev/v1/users";
+const proposalsApiBase = "http://api.trustdemocracy.dev/v1/proposals";
+const socialApiBase = "http://api.trustdemocracy.dev/v1/social";
+
 export const environment = {
   production: false,
-  usersApi: "http://api.trustdemocracy.dev/v1/users",
-  proposalsApi: "http://api.trustdemocracy.dev/v1/proposals",
-  socialApi: "http://api.trustdemocracy.dev/v1/social"
+  usersApi: {
+    getToken: usersApiBase + "/token",
+    refreshToken: usersApiBase + "/token/refresh",
+
+    createUser: usersApiBase + "/users",
+    findUser: usersApiBase + "/users/:userId",
+    updateUser: usersApiBase + "/users/:userId",
+    deleteUser: usersApiBase + "/users/:userId"
+  },
+  proposalsApi: {
+    createProposal: proposalsApiBase + "/proposals",
+    getProposal: proposalsApiBase + "/proposals/:id",
+    deleteProposal: proposalsApiBase + "/proposals/:id",
+    publishProposal: proposalsApiBase + "/proposals/:proposalId/publish",
+    unpublishProposal: proposalsApiBase + "/proposals/:proposalId/unpublish",
+
+    createComment: proposalsApiBase + "/proposals/:proposalId/comments",
+    getComments: proposalsApiBase + "/proposals/:proposalId/comments",
+    deleteComment: proposalsApiBase + "/proposals/:proposalId/comments/:commentId",
+    voteComment: proposalsApiBase + "/proposals/:proposalId/comments/:commentId",
+  },
+  socialApi: {
+    getEvents: socialApiBase + "/events",
+    getEventsByUser: socialApiBase + "/events/:userId",
+
+    followUser: socialApiBase + "/follow",
+    unFollow: socialApiBase + "/follow",
+    acceptFollow: socialApiBase + "/follow/accept",
+    cancelFollow: socialApiBase + "/follow/cancel",
+    getFollowRequests: socialApiBase + "/follow/requests",
+
+    trustUser: socialApiBase + "/trust",
+    unTrust: socialApiBase + "/trust",
+    acceptTrust: socialApiBase + "/trust/accept",
+    cancelTrust: socialApiBase + "/trust/cancel",
+    getTrustRequests: socialApiBase + "/trust/requests"
+  }
 };
