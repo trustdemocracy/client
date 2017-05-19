5f4df530c0d54dae1b5b448e4b94d492408f25c3
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2017-05-19 06:13:12 +0200

    Update AuthenticationService

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -7,22 +7,51 @@ import { environment } from 'app/../environments/environment';
 
 @Injectable()
 export class AuthenticationService {
-  private readonly STORAGE_TOKEN_KEY: string = 'userToken';
-  private currentToken: string;
+  private readonly ACCESS_TOKEN_KEY: string = 'userToken';
+  private readonly REFRESH_TOKEN_KEY: string = 'refreshToken';
+  private accessToken: string;
+  private refreshToken: string;
+  private remember: boolean;
 
   constructor(private http: Http) {
-    this.currentToken = this.getStoredToken();
+    this.accessToken = this.getAccessToken();
+    this.refreshToken = this.getRefreshToken();
   }
 
   login(user: User, remember: boolean = false): Observable<boolean> {
-    return this.http.post(environment.usersApi.getToken,
-      JSON.stringify({ username: user.username, password: user.password }))
+    const content: string = JSON.stringify({ username: user.username, password: user.password });
+
+    return this.http.post(environment.usersApi.getToken, content)
       .map((response: Response) => {
-        if (response.ok) {
-          const token = response.json() && response.json().token;
-          if (token) {
-            this.currentToken = token;
-            this.storeToken(token, remember);
+        if (response.ok && response.json()) {
+          const accessToken = response.json().accessToken;
+          const refreshToken = response.json().refreshToken;
+          if (accessToken && refreshToken) {
+            this.accessToken = accessToken;
+            this.refreshToken = refreshToken;
+            this.remember = remember;
+            this.storeToken(accessToken, refreshToken, remember);
+            return true;
+          }
+        }
+
+        return false;
+      });
+  }
+
+  refreshAccessToken(): Observable<boolean> {
+    const content: string = JSON.stringify({ refreshToken: this.refreshToken });
+    const headers: Headers = this.addAuthHeader(new Headers());
+
+    return this.http.post(environment.usersApi.refreshToken, content, headers)
+      .map((response: Response) => {
+        if (response.ok && response.json()) {
+          const accessToken = response.json().accessToken;
+          const refreshToken = response.json().refreshToken;
+          if (accessToken && refreshToken) {
+            this.accessToken = accessToken;
+            this.refreshToken = refreshToken;
+            this.storeToken(accessToken, refreshToken, this.remember);
             return true;
           }
         }
@@ -32,36 +61,59 @@ export class AuthenticationService {
   }
 
   isLogged(): boolean {
-    return this.currentToken !== null;
+    return this.accessToken !== null;
   }
 
   logout(): void {
-    this.currentToken = null;
-    localStorage.removeItem(this.STORAGE_TOKEN_KEY);
-    this.deleteCookie(this.STORAGE_TOKEN_KEY);
+    this.accessToken = null;
+    this.removeTokens();
   }
 
-  private getStoredToken(): string {
-    let cookieToken = this.getCookie(this.STORAGE_TOKEN_KEY);
+  addAuthHeader(headers: Headers): Headers {
+    headers.append('Authorization', this.accessToken);
+    return headers;
+  }
+
+  private getAccessToken(): string {
+    let cookieToken = this.getCookie(this.ACCESS_TOKEN_KEY);
     if (cookieToken !== null) {
       return cookieToken;
     }
-    return localStorage.getItem(this.STORAGE_TOKEN_KEY);
+    return localStorage.getItem(this.ACCESS_TOKEN_KEY);
   }
 
-  private storeToken(token: string, remember: boolean): void {
-    if (remember) {
-      localStorage.setItem(this.STORAGE_TOKEN_KEY, token);
-    } else {
-      this.setCookie(this.STORAGE_TOKEN_KEY, token);
+  private getRefreshToken(): string {
+    let cookieToken = this.getCookie(this.REFRESH_TOKEN_KEY);
+    if (cookieToken !== null) {
+      return cookieToken;
     }
+
+    this.remember = true;
+    return localStorage.getItem(this.REFRESH_TOKEN_KEY);
+  }
+
+  private storeToken(accessToken: string, refreshToken: string, remember: boolean): void {
+    if (remember) {
+      localStorage.setItem(this.ACCESS_TOKEN_KEY, accessToken);
+      localStorage.setItem(this.REFRESH_TOKEN_KEY, refreshToken);
+    } else {
+      this.setCookie(this.ACCESS_TOKEN_KEY, accessToken);
+      this.setCookie(this.REFRESH_TOKEN_KEY, refreshToken);
+    }
+  }
+
+  private removeTokens() {
+    localStorage.removeItem(this.ACCESS_TOKEN_KEY);
+    localStorage.removeItem(this.REFRESH_TOKEN_KEY);
+    this.deleteCookie(this.ACCESS_TOKEN_KEY);
+    this.deleteCookie(this.REFRESH_TOKEN_KEY);
   }
 
   private setCookie(name: string, value: string): void {
     document.cookie = name + '=' + value;
   }
 
-  private deleteCookie(name:string): void {
+  private deleteCookie(name: string): void {
     document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   }
 
